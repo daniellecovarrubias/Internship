2a8712d27928bab50ed37eb8dee1e59237dc482f
Danielle Covarrubias <30283056+daniellecovarrubias@users.noreply.github.com>
2017-07-21 13:03:39 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,124 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+	
+	IDEAS:
+		how do you pronounce payman?
+		
+     */
+    "QUESTIONS_EN_GB" : [
+      
+    ],
+    "QUESTIONS_EN_US" : [
+        {
+            "When was Southland Industries founded?": [
+                "1949",
+                "1965",
+                "1952",
+                "2003",
+				"1897",
+				"1974",
+				"1983",
+				"1990"
+            ]
+        },
+        {
+            "Which one of these is not part of Southland": [
+                "Southland Controls",
+				"Southland Industries",
+                "Southland Energy",
+                "Southland Engineering",
+                "Envise"
+            ]
+        },
+        {
+            "Which of the following is not a phase of the Southland workflow?": [
+                "Service",
+                "Business Development",
+                "Sales",
+                "Design",
+				"Build",
+				"Closeout"
+            ]
+        },
+        {
+            "Who is the Southern California division leader of Southland?": [
+                "Payman Farrokhyar",
+                "Spongebob",
+                "He Who Must Not Be Named",
+                "Darth Vader"
+            ]
+        },
+        {
+            "Which of the following is not a Southland office location?": [
+                "Chicago",
+                "Garden Grove",
+                "San Diego",
+                "Washington D.C.",
+				"Union City",
+				"Las Vegas",
+				"Portland"
+            ]
+        },
+        {
+            "Who is the CEO of Southland?": [
+                "Ted Lynch",
+                "Will Ferrell",
+                "Jonah Hill",
+                "Tina Fey"
+            ]
+        },
+        {
+            "Where did Ted Lynch go to school?": [
+                "Penn State",
+                "Michigan State",
+                "Rutgers University",
+                "Ohio State",
+				"Purdue University",
+				"University of Illinois",
+				"Indiana University",
+				"University of Maryland",
+				"University of Wisconsin",
+				"University of Minnesota",
+				"Northwestern University",
+				"University of Iowa"
+            ]
+        },
+
+        {
+            "Which of the following is NOT a part of Southland's core purpose?": [
+                "retail",
+                "building design",
+                "construction",
+                "operation"
+            ]
+        },
+        {
+            "Which of the following is not a Southland core value?": [
+                "pessimism",
+                "people",
+                "integrity",
+                "collaboration",
+				"accountability",
+				"safety",
+				"innovation",
+				"sustainability"
+            ]
+        },
+        {
+            "Which of the following is not a part of Southland's big hairy audacious goal?": [
+                "Waste",
+                "Design",
+                "Build",
+                "Maintain",
+            ]
+        },
+        
+    ],
+    "QUESTIONS_DE_DE" : [
+        
+    ]
+};
